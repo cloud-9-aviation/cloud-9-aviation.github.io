3457c21bfc95301e864f6682c434901b0617a181
Alexander Ilyin <alexander@ilyin.eu>
2026-01-10 10:43:25 +0000

    Added Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,13 +7,16 @@ import tailwindcss from "@tailwindcss/vite";
 
 import icon from "astro-icon";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), icon()],
+  integrations: [react(), icon(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
   },
+  site: 'https://cloud9aviation.info',
   server: {
     allowedHosts: ["championless-caridad-toxophily.ngrok-free.dev"],
   },
